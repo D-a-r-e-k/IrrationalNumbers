352da9a98c3a1941ab5ef167f973baf513ae5723
coderplug <dovydas.jurenkovas@mif.stud.vu.lt>
2016-12-28 18:07:18 +0200

    Sine expansion

--- IrrationalNumbers/IrrationalNumbers.Logic/Expansions/SinTaylorExpansion.cs
+++ IrrationalNumbers/IrrationalNumbers.Logic/Expansions/SinTaylorExpansion.cs
@@ -1,34 +1,37 @@
 ﻿
+using System;
+
 namespace IrrationalNumbers.Logic.Expansions
 {
-    public class SinTaylorExpansion: IBasicFunctionExpansion
+    public class SinTaylorExpansion : IBasicFunctionExpansion
     {
 
-        //public RemainderResult EvaluateN(int wantedRemainder, double x)
-        //{
-        //    for (int i = 1; ; ++i)
-        //    {
-        //        double possibleRemainder = Math.Pow(x, 2 * i) / Utils.CalculateFactorial(2 * i);
+        public RemainderResult EvaluateN(int wantedRemainder, double x)
+        {
+            for (int i = 1;; ++i)
+            {
+                double possibleRemainder = Math.Pow(x, 2*i)/Utils.CalculateFactorial(2*i);
 
-        //        if (Math.Abs(possibleRemainder) < Math.Pow(10, wantedRemainder))
-        //            return new RemainderResult()
-        //            {
-        //                Remainder = possibleRemainder,
-        //                RemainderOrder = i
-        //            };
-        //    }
-        //}
+                if (Math.Abs(possibleRemainder) < Math.Pow(10, wantedRemainder))
+                    return new RemainderResult()
+                    {
+                        Remainder = possibleRemainder,
+                        RemainderOrder = i
+                    };
+            }
+        }
 
         public BigDecimal ExpandFunction(int wantedRemainder, double x)
         {
-            //    RemainderResult remainderResult = EvaluateN(wantedRemainder, x);
-
-            //    double result = remainderResult.Remainder + 1;
-            //    for (int i = 1; i <= remainderResult.RemainderOrder; ++i)
-            //        result += Math.Pow(-1, i-1) * Math.Pow(x, 2 * i) / Utils.CalculateFactorial(2 * i);
-
-            //    return result;
-            return 0.0;
+            RemainderResult remainderResult = EvaluateN(wantedRemainder, x);
+            BigDecimal result = remainderResult.Remainder;
+            for (int i = 1; i <= remainderResult.RemainderOrder; ++i)
+            {
+                BigDecimal ithElement = BigDecimal.PowBig(-1, i - 1)*BigDecimal.PowBig(x, 2*i-1)/
+                                        Utils.CalculateBigDecimalFactorial(2*i-1);
+                result += ithElement;
+            }
+            return result;
         }
     }
 }
