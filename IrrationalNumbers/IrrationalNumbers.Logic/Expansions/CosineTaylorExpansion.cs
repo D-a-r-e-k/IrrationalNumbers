60c75f9d145a1e926f9af4b4ec2ea61f799357d0
coderplug <dovydas.jurenkovas@mif.stud.vu.lt>
2016-12-28 18:33:31 +0200

    Changed sine, cosine class names, added tangent expansion

--- IrrationalNumbers/IrrationalNumbers.Logic/Expansions/CosineTaylorExpansion.cs
+++ IrrationalNumbers/IrrationalNumbers.Logic/Expansions/CosineTaylorExpansion.cs
@@ -1,17 +1,14 @@
-﻿
-using System;
+﻿using System;
 
 namespace IrrationalNumbers.Logic.Expansions
 {
-    public class SinTaylorExpansion : IBasicFunctionExpansion
+    public class CosineTaylorExpansion : IBasicFunctionExpansion
     {
-
         public RemainderResult EvaluateN(int wantedRemainder, double x)
         {
-            for (int i = 1;; ++i)
+            for (int i = 1; ; ++i)
             {
-                double possibleRemainder = Math.Pow(x, 2*i)/Utils.CalculateFactorial(2*i);
-
+                double possibleRemainder = Math.Pow(x, 2*i + 1) / Utils.CalculateFactorial(2*i + 1);
                 if (Math.Abs(possibleRemainder) < Math.Pow(10, wantedRemainder))
                     return new RemainderResult()
                     {
@@ -24,11 +21,12 @@ namespace IrrationalNumbers.Logic.Expansions
         public BigDecimal ExpandFunction(int wantedRemainder, double x)
         {
             RemainderResult remainderResult = EvaluateN(wantedRemainder, x);
-            BigDecimal result = remainderResult.Remainder;
+            BigDecimal result = remainderResult.Remainder + 1;
             for (int i = 1; i <= remainderResult.RemainderOrder; ++i)
             {
-                BigDecimal ithElement = BigDecimal.PowBig(-1, i - 1)*BigDecimal.PowBig(x, 2*i-1)/
-                                        Utils.CalculateBigDecimalFactorial(2*i-1);
+                BigDecimal ithElement = BigDecimal.PowBig(-1, i)*BigDecimal.PowBig(x, 2*i)/
+                                        Utils.CalculateBigDecimalFactorial(2*i);
+
                 result += ithElement;
             }
             return result;
--- IrrationalNumbers/IrrationalNumbers.Logic/Expansions/SineTaylorExpansion.cs
+++ IrrationalNumbers/IrrationalNumbers.Logic/Expansions/SineTaylorExpansion.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿
+using System;
 
 namespace IrrationalNumbers.Logic.Expansions
 {
-    public class CosTaylorExpansion : IBasicFunctionExpansion
+    public class SineTaylorExpansion : IBasicFunctionExpansion
     {
+
         public RemainderResult EvaluateN(int wantedRemainder, double x)
         {
-            for (int i = 1; ; ++i)
+            for (int i = 1;; ++i)
             {
-                double possibleRemainder = Math.Pow(x, 2*i + 1) / Utils.CalculateFactorial(2*i + 1);
+                double possibleRemainder = Math.Pow(x, 2*i)/Utils.CalculateFactorial(2*i);
+
                 if (Math.Abs(possibleRemainder) < Math.Pow(10, wantedRemainder))
                     return new RemainderResult()
                     {
@@ -21,12 +24,11 @@ namespace IrrationalNumbers.Logic.Expansions
         public BigDecimal ExpandFunction(int wantedRemainder, double x)
         {
             RemainderResult remainderResult = EvaluateN(wantedRemainder, x);
-            BigDecimal result = remainderResult.Remainder + 1;
+            BigDecimal result = remainderResult.Remainder;
             for (int i = 1; i <= remainderResult.RemainderOrder; ++i)
             {
-                BigDecimal ithElement = BigDecimal.PowBig(-1, i)*BigDecimal.PowBig(x, 2*i)/
-                                        Utils.CalculateBigDecimalFactorial(2*i);
-
+                BigDecimal ithElement = BigDecimal.PowBig(-1, i - 1)*BigDecimal.PowBig(x, 2*i-1)/
+                                        Utils.CalculateBigDecimalFactorial(2*i-1);
                 result += ithElement;
             }
             return result;
