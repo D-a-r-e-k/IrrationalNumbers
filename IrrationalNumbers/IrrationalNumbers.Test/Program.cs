6b8d5b1523d914208a5de0d7abb07219e04dbdef
Darius Butkevicius <darius.butkevicius.n@gmail.com>
2017-01-15 14:37:54 +0200

    exception handling added

--- IrrationalNumbers/IrrationalNumbers.Test/Program.cs
+++ IrrationalNumbers/IrrationalNumbers.Test/Program.cs
@@ -14,28 +14,34 @@ namespace IrrationalNumbers.Test
 
             do
             {
-                Console.Write("Please type expression for estimation (exit - to exit the program): ");
-                string expressionToBeEstimated = Console.ReadLine();
-
-                if (expressionToBeEstimated == "exit")
-                    break;
-
-                if (string.IsNullOrEmpty(expressionToBeEstimated))
+                try
                 {
-                    Console.WriteLine("Type valid expression");
-                    continue;
+                    Console.Write("Please type expression for estimation (exit - to exit the program): ");
+                    string expressionToBeEstimated = Console.ReadLine();
+
+                    if (expressionToBeEstimated == "exit")
+                        break;
+
+                    if (string.IsNullOrEmpty(expressionToBeEstimated))
+                    {
+                        Console.WriteLine("Type valid expression");
+                        continue;
+                    }
+
+                    Console.Write("Please type acceptable error (in format 1 / 10^x where x should be provided): ");
+                    var remainder = Console.ReadLine();
+                    int remainderParsed;
+                    if (!int.TryParse(remainder, out remainderParsed))
+                        Console.WriteLine("Acceptable error was provided not in correct format.");
+
+                    parser.ConfigureParser((-1) * remainderParsed);
+
+                    Console.WriteLine("Result: " + parser.Estimate(expressionToBeEstimated));
                 }
-
-                Console.Write("Please type acceptable error (in format 1 / 10^x where x should be provided): ");
-                var remainder = Console.ReadLine();
-                int remainderParsed;
-                if (!int.TryParse(remainder, out remainderParsed))
-                    Console.WriteLine("Acceptable error was provided not in correct format.");
-
-                parser.ConfigureParser((-1) * remainderParsed);
-                
-                Console.WriteLine("Result: " + parser.Estimate(expressionToBeEstimated));
-
+                catch (Exception)
+                {
+                    Console.WriteLine("Possibly not supported signature of a function was provided. Please check examples at 'Naudotojo vadovas' document.");
+                }
             } while (true);
         }
     }
